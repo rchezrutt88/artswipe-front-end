4a150cf0a7bd152d931c4fd9060ec61ced6f06e0
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-28 22:13:37 -0500

    write parser for raw urls to image url

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,18 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+let artData;
+
+let getImageUrl = function (userID) {
+  $.ajax()
+}
+
+let postImage = function (url) {
+
+}
+
+let parseImageUrl = function (rawUrl) {
+  let lessRaw = rawUrl.replace('/html/', '/art/').replace('.html', '.jpg')
+  return lessRaw
+}
