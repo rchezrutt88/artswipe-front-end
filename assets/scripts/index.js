91c2ea0d5f9eea3a608c44ab40583dcba9260f2a
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-29 00:36:09 -0500

    ajax request returns random image from db

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,16 +6,39 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 
+//TODO remember to change this when deployed...
+let baseUrl ='http://localhost:3000'
+
 let artData;
 
-let getImageUrl = function (userID) {
-  $.ajax()
-}
+let userData;
 
-let postImage = function (rawUrl) {
-  let imageUrl = parseImageUrl(rawUrl);
-  $(".image-container").append("<img src=" + imageUrl + " class='img-rounded' alt='http://www.wga.hu/' width='50%' height='50%'>")
-}
+let signUp = function(formData) {
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "/sign-up",
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(responseData) {
+    console.log(responseData);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+
+let getRandomImage = function () {
+
+  $.ajax({
+    type: "GET",
+    url: baseUrl + "/arts/random"
+  }).done(function(responseData) {
+    console.log(responseData)
+  }).fail(function(jqxhr) {
+    console.error(jqxhr)
+  });
+};
 
 let parseImageUrl = function (rawUrl) {
   let lessRaw = rawUrl.replace('/html/', '/art/').replace('.html', '.jpg')
@@ -23,6 +46,28 @@ let parseImageUrl = function (rawUrl) {
 }
 
 
+let postImage = function (rawUrl) {
+  let imageUrl = parseImageUrl(rawUrl);
+  $(".image-container").append("<img src=" + imageUrl + " class='img-rounded' alt='http://www.wga.hu/' width='50%' height='50%'>")
+}
+
+
+
+/*######################### EXECUTING CODE ###################################*/
+
+$(function() {
+
 $("#getImage").on('click', function() {
   postImage("http://www.wga.hu/html/h/hitchcoc/thebride.html")
+});
+
+$("#signupForm").on('submit', function(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  signUp(formData);
+  //let formData = new FormData(event.)
+});
+
+getRandomImage();
+
 });
