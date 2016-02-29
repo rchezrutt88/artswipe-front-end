8bf5b41ac62e6d6d2284fbbc2824a7e0cb9b3b25
RobertRuttenberg <rdruttenberg@gmail.com>
2016-02-29 09:49:39 -0500

    sign-up, login, and sign-out all work

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,7 +10,6 @@ require('./example');
 let baseUrl ='http://localhost:3000'
 
 let artData;
-
 let userData;
 
 let parseImageUrl = function (rawUrl) {
@@ -27,21 +26,6 @@ let clearImage = function() {
   $(".image-container").empty();
 };
 
-let signUp = function(formData) {
-  $.ajax({
-    type: "POST",
-    url: baseUrl + "/sign-up",
-    contentType: false,
-    processData: false,
-    data: formData,
-  }).done(function(responseData) {
-    console.log(responseData);
-  }).fail(function(jqxhr) {
-    console.error(jqxhr);
-  });
-};
-
-
 let getRandomImage = function () {
 
   $.ajax({
@@ -57,6 +41,77 @@ let getRandomImage = function () {
   });
 };
 
+let signUp = function(formData) {
+  $.ajax({
+    type: "POST",
+    url: baseUrl + "/sign-up",
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(responseData) {
+    console.log(responseData);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+let signIn = function(formData) {
+
+  //TODO handle cases where user already signed in
+
+  if (userData) {
+    throw 'user already signed in';
+  }
+
+  $.ajax({
+    type: "POST",
+    url: baseUrl + '/sign-in',
+    contentType: false,
+    processData: false,
+    data: formData,
+  }).done(function(responseData) {
+    console.log(responseData);
+
+    userData = responseData.user;
+    let userEmail = responseData.user.email;
+
+    //display user email in navbar
+    $("#leftBar").append("<li><p class='navbar-text'>Signed in as " + userEmail + "</p></li>");
+
+    //hide modal
+    $("#signinModal").modal("hide");
+
+
+  }).fail(function(jQXHR) {
+    console.log(jQXHR);
+  });
+};
+
+
+let signOut = function() {
+  //REQUIRES A TOKEN HEADER
+  // if (!userData) {
+  //   throw "no user signed in"
+  // }
+  console.log(userData.token);
+  $.ajax({
+    headers: {
+      Authorization: 'Token token=' + userData.token,
+    },
+    type: "DELETE",
+    url: baseUrl + "/sign-out/" + userData.id,
+
+  }).done(function(responseData) {
+    console.log(responseData);
+
+    //remove user details from nav bar
+    $("#leftBar").empty();
+    //clear userData
+    userData = undefined;
+  }).fail(function(jQXHR) {
+    console.log(jQXHR);
+  });
+};
 
 
 
@@ -69,6 +124,7 @@ $("#getImage").on('click', function() {
   getRandomImage();
 });
 
+//for sign-up
 $("#signupForm").on('submit', function(event) {
   event.preventDefault();
   let formData = new FormData(event.target);
@@ -76,5 +132,20 @@ $("#signupForm").on('submit', function(event) {
   //let formData = new FormData(event.)
 });
 
+//For sign-in
+$("#signinForm").on('submit', function(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  signIn(formData);
+
+});
+
+//for sign-out
+$("#signoutbtn").on('click', function() {
+  if (!userData) {
+    throw "no user signed in";
+  }
+  signOut();
+});
 
 });
