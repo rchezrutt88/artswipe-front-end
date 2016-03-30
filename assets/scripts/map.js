777bbb26b764041e80647b6159eacf9e172bda71
RobertRuttenberg <rdruttenberg@gmail.com>
2016-03-29 22:39:48 -0400

    add map file

--- assets/scripts/map.js
+++ assets/scripts/map.js
@@ -1,13 +1,55 @@
 'use strict';
 
 let GoogleMapsLoader = require('google-maps');
+let myApp = require('./myApp')
 
-GoogleMapsLoader.load(function(google) {
-	new google.maps.Map(el, options);
-});
+let map;
+let geocoder;
+let BOSTON;
+
+
+GoogleMapsLoader.KEY = myApp.GM_API_KEY;
+GoogleMapsLoader.REGION = 'EU';
+GoogleMapsLoader.LIBRARIES = ['geometry'];
+
+let initializeMap = function() {
+  GoogleMapsLoader.load(function(google) {
+    map = new google.maps.Map($("#map")[0], {
+      center: {
+        lat: 42.3386,
+        lng: -71.0941
+      },
+      zoom: 15,
+    });
+		geocoder = new google.maps.Geocoder();
+  });
+};
 
 GoogleMapsLoader.onLoad(function(google) {
-	console.log('I just loaded google maps api');
+  console.log('I just loaded google maps api');
+  BOSTON = new google.maps.LatLng({lat: 42.2129, lng: -71.0349});
 });
 
-console.log('fart');
+let codeAddress = function(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log(getMiles(google.maps.geometry.spherical.computeDistanceBetween(BOSTON, results[0].geometry.location)));
+        map.setCenter(results[0].geometry.location);
+        let marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        console.log("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  };
+
+  function getMiles(i) {
+       return i*0.000621371192;
+  }
+
+module.exports = {
+  initializeMap,
+	codeAddress,
+}
